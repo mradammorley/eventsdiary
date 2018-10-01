fe3826a1562526b0f8cf69a1161452ab847e2121
Adam Morley <adam@adammorley.co.uk>
2018-10-01 07:46:11 +0100

    Comibined reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { events } from './events.reducer';
 import { calendar } from './calendar.reducer';
 
 const rootReducer = combineReducers({
+    events,
     calendar
 });
 
